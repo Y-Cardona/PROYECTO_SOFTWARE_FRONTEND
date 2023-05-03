dc4eae52b4c287901402a70fea86fd9e07471392
Y-Cardona <91815987+Y-Cardona@users.noreply.github.com>
2023-05-03 00:20:25 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = () => {
     return {
-        SERVER_URL: "master.djag71tz0493f.amplifyapp.com",
+        SERVER_URL: "back-software.herokuapp.com",
         // SERVER_URL: "http://localhost:3000",
     };
 };
