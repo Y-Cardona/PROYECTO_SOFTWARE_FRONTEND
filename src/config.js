7af453dcd039c63494711bf3da1b21c74cb8b63b
Juan Pinzon <jcpinzonc@uqvirtual.edu.co>
2023-05-03 00:13:01 -0500

    Prueba despliegue

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = () => {
-  return {
-    SERVER_URL: "http://localhost:8080",
-    // SERVER_URL: "http://localhost:3000",
-  };
+    return {
+        SERVER_URL: "master.djag71tz0493f.amplifyapp.com",
+        // SERVER_URL: "http://localhost:3000",
+    };
 };
 
 export default config;
